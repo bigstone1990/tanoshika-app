78154aa0f7a81aef0ccbed3896e839647b774691
bigstone1990 <masatakalman@gmail.com>
2025-05-02 14:38:33 +0900

    Userモデル ソフトデリート対応

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,8 +18,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->tinyInteger('authority');
+            $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
