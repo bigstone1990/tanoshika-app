cb10c808454f4bb29e9be2a57d8e35d2c7653b0c
bigstone1990 <masatakalman@gmail.com>
2025-05-14 11:35:13 +0900

    メンバー報告書日報 Index作成

--- app/Http/Controllers/Staff/MemberDailyReportController.php
+++ app/Http/Controllers/Staff/MemberDailyReportController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Member;
+use App\Models\DailyReport;
 
 class MemberDailyReportController extends Controller
 {
-    //
+    public function index(Member $member)
+    {
+        $memberId = $member->id;
+
+        $getMember = Member::select('id', 'name')->findOrFail($memberId);
+
+        $reports = DailyReport::select('id', 'date', 'submitted_at as submittedAt', 'staff_memo as staffMemo')
+            ->where('member_id', $memberId)
+            ->whereNotNull('submitted_at')
+            ->orderBy('date', 'desc')
+            ->paginate(5);
+
+        return Inertia::render('Staff/MemberReport/DailyReport/Index', [
+            'member' => $getMember,
+            'reports' => $reports,
+        ]);
+    }
 }
---
+++ resources/js/Pages/Staff/MemberReport/DailyReport/Index.tsx
@@ -0,0 +1,145 @@
+import AuthenticatedLayout from '@/Layouts/StaffAuthenticatedLayout';
+import { Head, Link } from '@inertiajs/react';
+import styled from 'styled-components'
+import { Paginate } from '@/types'
+import Pagination from '@/Components/Pagination'
+
+type Report = {
+  id: string;
+  date: string;
+  submittedAt: string;
+  staffMemo: string;
+};
+
+type IndexProps = {
+  member: {
+    id: number;
+    name: string;
+  };
+  reports: Paginate<Report>;
+};
+
+const StyledMemberReportDailyReportTableContainer = styled.div`
+  max-width: calc(6rem + 10rem + 12rem + 6rem + 1rem + 1px);
+  overflow-x: auto;
+  overflow-y: auto;
+  margin-left: auto;
+  margin-right: auto;
+
+  table {
+    border-collapse: separate;
+    border-spacing: 0;
+    thead {
+      tr {
+        th {
+          overflow-wrap: break-word;
+        }
+
+        &:first-of-type {
+          th {
+            &::before {
+              content: "";
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              border-right: 1px solid white;
+            }
+
+            &:first-of-type {
+              position: sticky;
+              top: 0;
+              left: 0;
+              z-index: 3;
+            }
+
+            &:not(:first-of-type) {
+              position: sticky;
+              top: 0;
+              z-index: 2;
+            }
+          }
+        }
+      }
+    }
+
+    tbody {
+      tr {
+        td {
+          overflow-wrap: break-word;
+
+          &:first-of-type {
+            position: sticky;
+            left: 0;
+            z-index: 1;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function Index({member, reports}: IndexProps) {
+  return (
+    <AuthenticatedLayout
+      header={
+        <h2 className="text-xl font-semibold leading-tight text-gray-800">
+          {member.name} さんの日報一覧
+        </h2>
+      }
+    >
+      <Head title={`${member.name} さんの日報一覧`} />
+
+      <div className="PageContainer">
+        <div className="PageWrapper">
+          <div className="Page">
+            <div className="PageContent">
+              <section className="text-gray-600">
+                <div className="container mx-auto">
+                  <div className="w-full mx-auto overflow-auto">
+                    <StyledMemberReportDailyReportTableContainer>
+                      <table className="table-fixed w-full text-left whitespace-normal">
+                        <thead>
+                          <tr>
+                            <th className="w-24 px-4 py-3 title-font tracking-wider font-medium text-gray-100 text-sm bg-gray-900 rounded-tl rounded-bl">操作</th>
+                            <th className="w-40 px-4 py-3 title-font tracking-wider font-medium text-gray-100 text-sm bg-gray-900">日付</th>
+                            <th className="w-48 px-4 py-3 title-font tracking-wider font-medium text-gray-100 text-sm bg-gray-900">提出日時</th>
+                            <th className="w-24 px-4 py-3 title-font tracking-wider font-medium text-gray-100 text-sm bg-gray-900 rounded-tr rounded-br">メモ有無</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {reports.data.length !== 0 ?
+                            reports.data.map(report => (
+                              <tr key={ report.id }>
+                                <td className="border-b-2 px-4 py-3 bg-white">
+                                  <Link as="button" href={route('staff.memberDailyReports.show', {member: member.id, date: report.date})} className="w-full text-white bg-sky-500 border-0 py-2 px-4 hover:bg-sky-600 rounded">詳細</Link>
+                                </td>
+                                <td className="border-b-2 px-4 py-3 bg-white">{report.date}</td>
+                                <td className="border-b-2 px-4 py-3 bg-white">{report.submittedAt}</td>
+                                <td className="border-b-2 px-4 py-3 bg-white">{report.staffMemo ? '○' : ''}</td>
+                              </tr>
+                            )) :
+                            <tr>
+                              <td className="border-b-2 px-4 py-3 bg-white"></td>
+                              <td className="border-b-2 px-4 py-3 bg-white">日報がありません</td>
+                              <td className="border-b-2 px-4 py-3 bg-white"></td>
+                              <td className="border-b-2 px-4 py-3 bg-white"></td>
+                            </tr>
+                            }
+                        </tbody>
+                      </table>
+                    </StyledMemberReportDailyReportTableContainer>
+                  </div>
+                  <div className="flex justify-center items-center mt-4">
+                    <Pagination links={reports.links} />
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  )
+}
